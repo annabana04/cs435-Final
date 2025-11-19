9be1e722d90b4f38e656f39e8ff5f635389536c1
Anna Meuse <annabana@salem.cs.colostate.edu>
2025-11-19 16:38:05 -0700

    baseline

---
+++ rank/data.java
@@ -0,0 +1,15 @@
+package rank;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.*;
+
+import org.apache.hadoop.io.BytesWritable;
+
+public class data {
+    
+}
---
+++ rank/main.java
@@ -0,0 +1,15 @@
+package rank;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+
+import pagerank.Book;
+import scala.Tuple2;
+import java.io.IOException;
+import java.util.*;
+
+
+public class main {
+    
+}
