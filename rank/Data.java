432740da6a5e1e82093a526a79fbbbe045a350c7
Quinten Roberts <quintenr@denver.cs.colostate.edu>
2025-12-02 13:42:58 -0700

    working

--- rank/Data.java
+++ rank/Data.java
@@ -16,10 +16,10 @@ public class Data
 
     public static class Doc 
     {
-        public final JavaPairRDD<Integer, List<Integer>> violence;
-        public final JavaPairRDD<Integer, List<Integer>> life;
+        public final JavaPairRDD<Integer, Double> violence;
+        public final JavaPairRDD<Integer, Double> life;
 
-        public Doc(JavaPairRDD<Integer, Integer> violence, JavaPairRDD<Integer, Integer> life)
+        public Doc(JavaPairRDD<Integer, Double> violence, JavaPairRDD<Integer, Double> life)
         {
             this.violence = violence;
             this.life = life;
@@ -29,19 +29,19 @@ public class Data
     public static Doc mapPairs(JavaSparkContext sc, String dataPath) 
     {
         JavaRDD<String> data = sc.textFile(dataPath);
-        JavaPairRDD<Integer, Integer> violencePair = data.mapToPair(s -> 
-        {
+        JavaPairRDD<Integer, Double> violencePair = data.mapToPair(s ->
+        { 
             String[] line = s.split(",");
-            int year = Integer.parseInt(line[3].trim());
-            int violence = Integer.parseInt(line[8].trim());
+            int year = Integer.parseInt(line[0].trim());
+            double violence = Double.parseDouble(line[1].trim());
             return new Tuple2<>(year, violence); // takes the year and pairs it with the violence score (year, violence score)
         });
 
-        JavaPairRDD<Integer, Integer> lifePair = data.mapToPair(s -> 
+        JavaPairRDD<Integer, Double> lifePair = data.mapToPair(s -> 
         {
             String[] line = s.split(",");
-            int year = Integer.parseInt(line[3].trim());
-            int life = Integer.parseInt(line[9].trim());
+            int year = Integer.parseInt(line[0].trim());
+            double life = Double.parseDouble(line[2].trim());
             return new Tuple2<>(year, life); // takes the year and pairs it with the violence score (year, violence score)
         });
 
